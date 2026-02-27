04c445077e5f449f9e188d2624f2751fd413167c
sammy <44491450+samuelkanyingi@users.noreply.github.com>
2026-02-27 07:44:44 +0300

    Fix media file serving in production

--- fashion_project/urls.py
+++ fashion_project/urls.py
@@ -19,10 +19,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("shop.urls")),
     path("api/", include("shop.urls")),
     path("api/", include("shop.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve media files in production
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        path("media/<path:path>", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
